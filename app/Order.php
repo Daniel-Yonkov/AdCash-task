53c4e5ae282dc639146f3a837360fc04c5c56cd1
Daniel-Yonkov <daniel.yonkov@gmail.com>
2017-07-12 15:49:02 +0300

    filtering optimized, display order bug fixed

--- app/Order.php
+++ app/Order.php
@@ -13,6 +13,8 @@ class Order extends Model
     protected $discount =0;
     //used in filtering based on product name
     protected $orders_id=array();
+    //relationship defined array for automatic filtering;
+    static $relationships=array('user','products');
 
     public function products()
     {
@@ -37,13 +39,12 @@ class Order extends Model
 
     public function edit(User $user, Collection $products, array $quantity)
     {
-        $order= clone $this;
         $this->getOrderParams($products, $quantity);
         $this->user()->associate($user);
-        $this->updateQuantity($products, $order, $quantity);
         $this->attributes['total']=$this->total;
         $this->attributes['discount']=$this->discount;
         $this->save();
+        $this->updateQuantity($products,$quantity);
     }
 
     protected function addQuantity(Collection $products, Order $order,  array $quantity)
@@ -54,14 +55,14 @@ class Order extends Model
         }
     }
 
-     protected function updateQuantity(Collection $products, Order $order,  array $quantity)
+     protected function updateQuantity(Collection $products, array $quantity)
     {
          foreach ($products as $product) {
-            if(!$order->products()->updateExistingPivot($product->id, ['quantity' => $quantity[$product->name]])){
-                $order->products()->attach($product, ['quantity' => $quantity[$product->name]]);
+            if(!$this->products()->updateExistingPivot($product->id, ['quantity' => $quantity[$product->name]])){
+                $this->products()->attach($product, ['quantity' => $quantity[$product->name]]);
             }
         }
-        $order->products()->sync($products);
+        $this->products()->sync($products);
     }
 
     protected function getOrderParams(Collection $products, array $quantity)
@@ -79,3 +80,39 @@ class Order extends Model
         $this->discount = $discount;
         $this->total = $total;
     }
+
+    public static function orderFilter($term=null, Carbon $time = null){
+        $orders = self::orderBy('updated_at','desc')->get();
+        $relation = null;
+        $filter = null;
+
+        if($term) {
+            if((User::where('name',$term)->get())->isNotEmpty()){
+                $relation = 'user';
+                $filter = 'name';
+            }
+            elseif((Product::where('name', $term)->get())->isNotEmpty()){
+                $relation = 'products';
+                $filter = 'name';
+            }
+            else {
+                return collect([]);
+            }
+            $orders = Order::whereHas($relation, function($query) use($filter, $term){
+                $query->where($filter, $term);
+                })->orderBy('updated_at','desc')->get();
+        }
+        if($time){
+            $orders = $orders->where('updated_at','>=',$time);
+        }
+
+        return self::filteredResults($orders, $relation, $filter, $term);
+    }
+
+    protected static function filteredResults(Collection $orders, $relation, $filter, $term){
+            return$orders->each(function($order){
+                $order->load(self::$relationships);
+            });
+    }
+
+}
